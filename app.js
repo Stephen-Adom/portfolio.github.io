a6ca403293271364346db61b0ae64ec1de6c7fdd
I am Alaska <stephen.alaska94@hotmail.com>
2023-01-30 15:38:52 +0000

    add js linters to project

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-const navBtn = document.querySelector(".open-nav");
-const mobileNavigation = document.querySelector(".mobile-navigation");
-const navToggleBtn = document.querySelector(".nav-toggle-btn");
-const sidebarMenuContent = document.querySelector(".sidebar-menu");
-const navigationBar = document.querySelector("#navbar");
+const navBtn = document.querySelector('.open-nav');
+const mobileNavigation = document.querySelector('.mobile-navigation');
+const navToggleBtn = document.querySelector('.nav-toggle-btn');
+const sidebarMenuContent = document.querySelector('.sidebar-menu');
+const navigationBar = document.querySelector('#navbar');
 
-const infoSections = document.querySelectorAll(".info-section");
-const mainNavigationLinks = document.querySelectorAll(".nav-link");
+const infoSections = document.querySelectorAll('.info-section');
+const mainNavigationLinks = document.querySelectorAll('.nav-link');
 
-document.addEventListener("DOMContentLoaded", (e) => {
+function removeAllActiveClasses() {
+  Array.from(mainNavigationLinks).forEach((link) => {
+    link.classList.remove('active');
+  });
+}
+
+function setActiveClass(index) {
+  Array.from(mainNavigationLinks)[index].classList.add('active');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
   const sectionMargin = 100;
   let currentActive = 0;
 
-  window.addEventListener("scroll", (e) => {
-    const current =
-      infoSections.length -
-      Array.from(infoSections)
-        .reverse()
-        .findIndex(
-          (section) => window.scrollY >= section.offsetTop - sectionMargin
-        ) -
-      1;
+  window.addEventListener('scroll', () => {
+    const current = infoSections.length - [...infoSections]
+      .reverse()
+      .findIndex((section) => window.scrollY >= section.offsetTop - sectionMargin) - 1;
 
     if (current !== currentActive) {
       removeAllActiveClasses();
@@ -28,33 +33,23 @@ document.addEventListener("DOMContentLoaded", (e) => {
     }
 
     if (window.scrollY > 150) {
-      navigationBar.classList.add("navbar-shadow");
+      navigationBar.classList.add('navbar-shadow');
     } else {
-      navigationBar.classList.remove("navbar-shadow");
+      navigationBar.classList.remove('navbar-shadow');
     }
   });
 });
 
-function removeAllActiveClasses() {
-  Array.from(mainNavigationLinks).forEach((link) => {
-    link.classList.remove("active");
-  });
-}
-
-function setActiveClass(index) {
-  Array.from(mainNavigationLinks)[index].classList.add("active");
-}
-
 function openToggleClass(e) {
   e.stopPropagation();
-  mobileNavigation.classList.toggle("open-menu");
+  mobileNavigation.classList.toggle('open-menu');
 }
 
-navBtn.addEventListener("click", openToggleClass);
-navToggleBtn.addEventListener("click", openToggleClass);
+navBtn.addEventListener('click', openToggleClass);
+navToggleBtn.addEventListener('click', openToggleClass);
 
-sidebarMenuContent.addEventListener("click", (e) => {
-  if (e.target.tagName === "A") {
+sidebarMenuContent.addEventListener('click', (e) => {
+  if (e.target.tagName === 'A') {
     openToggleClass(e);
   }
 });
